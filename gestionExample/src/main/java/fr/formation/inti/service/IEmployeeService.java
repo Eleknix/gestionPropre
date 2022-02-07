58270d0e655da74c0e559a9cf1ab3ddb0d1a9768
aemad <aemad@LAPTOP-I503AC1V>
2022-02-07 14:42:28 +0300

    Configuration Spring

--- gestionExample/src/main/java/fr/formation/inti/service/IEmployeeService.java
+++ gestionExample/src/main/java/fr/formation/inti/service/IEmployeeService.java
@@ -1,5 +1,16 @@
 package fr.formation.inti.service;
 
-public interface IEmployeeService {
+import java.util.List;
+import fr.formation.inti.entity.Employee;
 
+public interface IEmployeeService {
+	Integer save(Employee employee);
+
+	void update(Employee employee);
+
+	void delete(Integer id);
+
+	Employee findById(Integer id);
+
+	List<Employee> findAll();
 }
--- gestionExample/src/main/java/fr/formation/inti/service/IUserService.java
+++ gestionExample/src/main/java/fr/formation/inti/service/IUserService.java
@@ -3,10 +3,7 @@ package fr.formation.inti.service;
 import fr.formation.inti.entity.User;
 
 public interface IUserService {
-	
-	
-	User findByLoginAndPassword(String login, String password) ;
-	
-	Integer ajouterUser(User user);
+	User findByLoginAndPassword(String login, String password);
 
+	Integer ajouterUser(User user);
 }
